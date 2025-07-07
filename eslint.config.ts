e75560b5e1df4f11b37ffbcf26e74d509f95910b
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-07-07 18:50:11 -0300

    chore: add eslint-import and config eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,27 @@
 import love from 'eslint-config-love'
 import prettier from 'eslint-config-prettier'
-import { defineConfig } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
 
 export default [
   {
-    files: ['src/**/*.ts'],
+    files: ['**/*.ts'],
     ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
     languageOptions: {
       parserOptions: {
         project: './tsconfig.json',
       },
     },
-    ...love,
-    ...prettier,
+    rules: {
+      'import/no-unresolved': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/class-methods-use-this': 'off',
+    },
+    plugins: {
+      import: importPlugin,
+    },
   },
+  love,
+  prettier,
 ]
