b46296724a42afdd6d9f92621b19fe0d8bf5fb35
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-07-07 23:30:27 -0300

    refactor: add customized error for missing param

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['src'],
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
